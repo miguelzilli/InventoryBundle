4f621e101b89a46b07fd76a3da3efaa92c7f092c
Miguel Zilli <miguelzilli@gmail.com>
2013-05-26 01:31:16 -0300

    Added Condition Entity

---
+++ src/mz/InventoryBundle/Entity/Condition.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace mz\InventoryBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * Condition
+ *
+ * @ORM\Table(name="conditions")
+ * @ORM\Entity
+ * @UniqueEntity(fields="name")
+ */
+class Condition
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="display", type="string", length=255)
+     */
+    private $display;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     *
+     * @ORM\OneToMany(targetEntity="mz\InventoryBundle\Entity\Item", mappedBy="condition")
+     */
+    private $items;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->items = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Condition
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set display
+     *
+     * @param string $display
+     * @return Condition
+     */
+    public function setDisplay($display)
+    {
+        $this->display = $display;
+
+        return $this;
+    }
+
+    /**
+     * Get display
+     *
+     * @return string 
+     */
+    public function getDisplay()
+    {
+        return $this->display;
+    }
+
+    /**
+     * Add items
+     *
+     * @param \mz\InventoryBundle\Entity\Item $items
+     * @return Condition
+     */
+    public function addItem(\mz\InventoryBundle\Entity\Item $items)
+    {
+        $this->items[] = $items;
+
+        return $this;
+    }
+
+    /**
+     * Remove items
+     *
+     * @param \mz\InventoryBundle\Entity\Item $items
+     */
+    public function removeItem(\mz\InventoryBundle\Entity\Item $items)
+    {
+        $this->items->removeElement($items);
+    }
+
+    /**
+     * Get items
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+}
